7734b64f823238b32ef814e46d98071a91529d7f
Saulo de Oliveira <143642628+SauloOliveiraDev@users.noreply.github.com>
2024-10-31 11:56:53 +0000

    Add colab1.py

---
+++ teste.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls || clear")
+
+print('Saulo')
+
+#Comandos do git
+#git pull (Atualiza o repositorio atual com dados do repositório no Github)
+#git add . (Adiciona novos dados para o commit.)
+#git commit -m "Titulo para o commit" (Versiona com os novos dados)
+#git pull (Atualiza trazendo commits do Github)
+#git push (Envia os commits para o GitHub)
